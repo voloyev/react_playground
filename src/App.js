562b8076fc042cbc82d465e89fa4b99815614f5c
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-11-23 17:12:17 +0200

    Add handler

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Person from './Person/Person.js';
+//42
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Max',
+        age: 23
+      },
+      {
+        name: 'Ann',
+        age: 23
+      },
+      {
+        name: 'Rob',
+        age: 23
+      }
+    ]
+  };
+
+  switchNameHandler = () => {};
+
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1> Hello! </h1>
-    //   </div>
-    // );
-    return React.createElement(
-      'div',
-      null,
-      React.createElement('h1', { className: 'App' }, 'Hello')
+    return (
+      <div className="App">
+        <h1> Hello! </h1>
+        <p> Here p </p>
+        <button onClick={this.switchNameHandler}>Switch name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobby: Noooo
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
     );
   }
 }
